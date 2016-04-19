c0341812e12e7611d2606f8a3f91644c3a7dcf83
ycobos <ycobos@uninorte.edu.co>
2016-04-18 21:41:33 -0500

    f

---
+++ app/src/main/java/com/augustosalazar/as_android_cardview/MainActivity.java
@@ -0,0 +1,121 @@
+package com.augustosalazar.as_android_cardview;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements ViewAdapter.RecyclerClickListner {
+
+    private ViewAdapter viewAdapter;
+    private RecyclerView mRecyclerView;
+    public String BaseDatos= "listview00";
+    public Firebase miFirebaseRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        miFirebaseRef = new Firebase("https://" +BaseDatos+ ".firebaseio.com/");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Button btncrear = (Button) findViewById(R.id.crearBtn);
+        btncrear.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.show();
+
+                final EditText mMessageEdit = (EditText) dialog.findViewById(R.id.DialogEditText);
+
+
+                Button AgregarBtn = (Button) dialog.findViewById(R.id.CrearBtn);
+                AgregarBtn.setOnClickListener(new View.OnClickListener() {
+
+
+                    @Override
+                    public void onClick(View v) {
+                        String message = mMessageEdit.getText().toString();
+                        Map<String,String> values = new HashMap<>();
+                        values.put("name", message);
+
+                        Firebase mFirebaseRef = new Firebase("https://" + BaseDatos + ".firebaseio.com/");
+                        mFirebaseRef.push().setValue(values);
+                        mMessageEdit.setText("");
+
+                    }
+                });
+
+                Button CancelarBtn = (Button) dialog.findViewById(R.id.CambiarBtn);
+                CancelarBtn.setOnClickListener(new View.OnClickListener() {
+
+
+                    @Override
+                    public void onClick(View v) {
+                        dialog.cancel();
+                    }
+                });
+
+
+            }
+        });
+
+        miFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                ArrayList<String> names = new ArrayList<String>();
+                ArrayList<String> keys = new ArrayList<String>();
+
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    names.add(postSnapshot.child("name").getValue().toString());
+                    keys.add(postSnapshot.getRef().getKey().toString());
+
+                }
+                String[] nombres = new String[names.size()];
+                nombres = names.toArray(nombres);
+                String[] llaves = new String[keys.size()];
+                llaves = keys.toArray(llaves);
+
+                viewAdapter = new ViewAdapter(this, nombres);
+                //viewAdapter.setRecyclerClickListner(this);
+                mRecyclerView.setAdapter(viewAdapter);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void itemClick(View view, int position) {
+        Log.d("Recyclerview", "Click position " + position);
+    }
+}
---
+++ app/src/main/java/com/augustosalazar/as_android_cardview/ViewAdapter.java
@@ -0,0 +1,77 @@
+package com.augustosalazar.as_android_cardview;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.MyViewHolder>{
+
+    private final Context context;
+    private LayoutInflater inflater;
+    private String[] data;
+    private RecyclerClickListner mRecyclerClickListner;
+
+    public ViewAdapter(ValueEventListener context,String[] data){
+        inflater = LayoutInflater.from(context);
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflater.inflate(R.layout.custom_row, parent, false);
+        MyViewHolder holder = new MyViewHolder(v);
+        return holder;
+        
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        String information = data[position];
+        holder.tv.setText(information);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public void setRecyclerClickListner(RecyclerClickListner recyclerClickListner){
+        mRecyclerClickListner = recyclerClickListner;
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView tv;
+
+        public MyViewHolder(View v) {
+            super(v);
+            v.setOnClickListener(this);
+            tv = (TextView) v.findViewById(R.id.listText);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mRecyclerClickListner != null) {
+                mRecyclerClickListner.itemClick(v, getPosition());
+            }
+        }
+    }
+
+    public interface RecyclerClickListner
+    {
+        public void itemClick(View view, int position);
+    }
+}
